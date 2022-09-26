e76af94a7b7e06ceca2f6ff8856ddfcd2ec7340f
Trung Nguyen <trungntdev@gmail.com>
2022-09-27 00:05:48 +0700

    log changes

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -69,12 +69,12 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            logger.warning('Invalid login attempt')
+            logger.warning('Invalid login attempt 1')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            app.logger.info('admin logged in successfully')
+            logger.warning('admin logged in successfully 1')
             next_page = url_for('home')
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
@@ -86,7 +86,7 @@ def authorized():
     if request.args.get('state') != session.get("state"):
         return redirect(url_for("home"))  # No-OP. Goes back to Index page
     if "error" in request.args:  # Authentication/Authorization failure
-        app.logger.error('Invalid login attempt')
+        app.logger.error('Invalid login attempt 2')
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
@@ -107,7 +107,7 @@ def authorized():
         user = User.query.filter_by(username="admin").first()
         login_user(user)
         _save_cache(cache)
-        app.logger.info('admin logged in successfully')
+        app.logger.error('admin logged in successfully 2')
     return redirect(url_for('home'))
 
 @app.route('/logout')
